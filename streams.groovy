11d1b50ba4d18814a81ad3529b2988b7841dfd7b
Jonathan Lebon <jonathan@jlebon.com>
2019-06-18 15:07:03 -0400

    pipeline: add prod, devel, and mechanical pipelines
    This adds pipeline buildconfigs for all the current streams (testing,
    testing-devel, and bodhi-updates). Rather than having a separate
    buildconfig for each stream, we just group them by prod, devel, and
    mechanical since the triggering within a particular group is likely
    going to be the same.
    
    One note here is that I removed the GitHub webhook from the OpenShift
    side, and instead made it part of the Jenkinsfile itself. This is
    because we need webhooks to Jenkins anyway to trigger off of
    `fedora-coreos-config`, so we might as well use the same mechanism for
    `fedora-coreos-pipeline` as well.
    
    Another note: I initially had the `prod` pipeline be solely manually
    triggered, though changed that later on to be triggered on pushes to
    `fedora-coreos-config`: I think it'd be cool if the only manual step
    required for a release was simply to push a commit to `testing`. Anyway,
    we can refine that later if needed (and all the other triggers as well).

--- streams.groovy
+++ streams.groovy
@@ -4,4 +4,41 @@ prod = ['testing' /* , 'stable', 'next' */]
 devel = ['testing-devel' /* , 'next-devel' */]
 mechanical = ['bodhi-updates' /* , 'bodhi-updates-testing', 'branched', 'rawhide' */]
 
+// Maps a list of streams to a list of GitSCM branches.
+def as_branches(streams) {
+    return streams.collect{ [name: "origin/${it}"] }
+}
+
+// Retrieves the stream name from a branch name.
+def from_branch(branch) {
+    assert branch.startsWith('origin/')
+    return branch['origin/'.length()..-1]
+}
+
+// Returns the default trigger for push notifications. This will trigger builds
+// when SCMs change (either the pipeline code itself, or fedora-coreos-config).
+def get_push_trigger() {
+    return [
+        // this corresponds to the "GitHub hook trigger for GITScm polling"
+        // checkbox; i.e. trigger a poll when a webhook event comes in at
+        // /github-webhook/ for the repo we care about
+        githubPush(),
+        // but still also force poll SCM every 30m as fallback in case hooks
+        // are down/we miss one
+        pollSCM('H/30 * * * *')
+    ]
+}
+
+// Returns true if the build was triggered by a push notification.
+def triggered_by_push() {
+    return (currentBuild.getBuildCauses('com.cloudbees.jenkins.GitHubPushCause').size() > 0)
+}
+
+// Starts a stream build.
+def build_stream(stream) {
+    // use `oc start-build` instead of the build step
+    // https://bugzilla.redhat.com/show_bug.cgi?id=1580468
+    sh "oc start-build fedora-coreos-pipeline -e STREAM=${stream}"
+}
+
 return this
