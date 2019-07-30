coreos/fedora-coreos-pipeline

commit d04e7119a34c260de4c6fdf23025cc697dac3af9

Jenkinsfile: run Publish step on master
To be able to start the release job, we need `oc` which is not in cosa.
Run that step on the master itself, just like the wrapper jobs do.

Jenkinsfile
@@ -64,6 +64,7 @@ if (official) {
     pod = pod.replace("COREOS_ASSEMBLER_IMAGE", "${developer_prefix}-coreos-assembler:master")
 }
 
+def newBuildID
 podTemplate(cloud: 'openshift', label: 'coreos-assembler', yaml: pod, defaultContainer: 'jnlp') {
     node('coreos-assembler') { container('coreos-assembler') {
 
@@ -152,7 +153,7 @@ podTemplate(cloud: 'openshift', label: 'coreos-assembler', yaml: pod, defaultCon
             """)
         }
 
-        def newBuildID = utils.shwrap_capture("readlink builds/latest")
+        newBuildID = utils.shwrap_capture("readlink builds/latest")
         if (prevBuildID == newBuildID) {
             currentBuild.result = 'SUCCESS'
             currentBuild.description = "[${params.STREAM}] 💤 (no new build)"
@@ -280,17 +281,20 @@ podTemplate(cloud: 'openshift', label: 'coreos-assembler', yaml: pod, defaultCon
               """)
             }
         }
-
-        // For now, we auto-release all non-production streams builds. That
-        // way, we can e.g. test testing-devel AMIs easily.
-        if (official && !(params.STREAM in streams.production)) {
-            stage('Publish') {
-                utils.shwrap("""
-                oc start-build --wait fedora-coreos-pipeline-release \
-                    -e STREAM=${params.STREAM} \
-                    -e VERSION=${newBuildID}
-                """)
-            }
-        }
     }}
+}
+
+// For now, we auto-release all non-production streams builds. That
+// way, we can e.g. test testing-devel AMIs easily.
+if (official && !(params.STREAM in streams.production)) {
+    stage('Publish') {
+        // use master, which has `oc` in it already
+        node {
+            utils.shwrap("""
+            oc start-build --wait fedora-coreos-pipeline-release \
+                -e STREAM=${params.STREAM} \
+                -e VERSION=${newBuildID}
+            """)
+        }
+    }
 }

commit 009c44a8f65eb5e7e460f7e380704c18fa8f46bd

Jenkinsfile: only run Publish on new builds
Minor regression from #115. Now that the `Publish` stage is outside the
main cosa `podTemplate`, it always executes, even if no new builds were
created. This is because the no-op `return` only returns from that
scope.

Fix this by only assigning to `newBuildID` if it's actually new. Then
gate the `Publish` step on that.

Jenkinsfile
@@ -153,13 +153,14 @@ podTemplate(cloud: 'openshift', label: 'coreos-assembler', yaml: pod, defaultCon
             """)
         }
 
-        newBuildID = utils.shwrap_capture("readlink builds/latest")
-        if (prevBuildID == newBuildID) {
+        def latestBuildID = utils.shwrap_capture("readlink builds/latest")
+        if (prevBuildID == latestBuildID) {
             currentBuild.result = 'SUCCESS'
             currentBuild.description = "[${params.STREAM}] 💤 (no new build)"
             return
         } else {
             currentBuild.description = "[${params.STREAM}] ⚡ ${newBuildID}"
+            newBuildID = latestBuildID
 
             // and insert the parent info into meta.json so we can display it in
             // the release browser and for sanity checking
@@ -286,7 +287,7 @@ podTemplate(cloud: 'openshift', label: 'coreos-assembler', yaml: pod, defaultCon
 
 // For now, we auto-release all non-production streams builds. That
 // way, we can e.g. test testing-devel AMIs easily.
-if (official && !(params.STREAM in streams.production)) {
+if (newBuildID && official && !(params.STREAM in streams.production)) {
     stage('Publish') {
         // use master, which has `oc` in it already
         node {