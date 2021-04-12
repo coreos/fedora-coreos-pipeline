aac07b0dc3cd3fd0669eeccd1546907471b6745e
Dusty Mabe <dusty@dustymabe.com>
2021-04-12 09:33:32 -0400

    streams: disable mechanical streams for now
    We've got plenty of CI failures that we need to sort through. Let's
    disable these streams to decrease the noise for now.

--- streams.groovy
+++ streams.groovy
@@ -2,7 +2,7 @@
 
 production = ['testing', 'stable', 'next']
 development = ['testing-devel', 'next-devel']
-mechanical = ['rawhide', 'branched' /*'bodhi-updates', 'bodhi-updates-testing' */]
+mechanical = [/* 'rawhide', 'branched', 'bodhi-updates', 'bodhi-updates-testing' */]
 
 all_streams = production + development + mechanical
 
