coreos/fedora-coreos-pipeline

commit 8bae67d8877bdec461d1fa8607c5d0e2f0882170

jobs/build-node-image: fix skopeo arch mapping for aarch64
The skopeo command expects Go-style architecture names like amd64 and
arm64. Use the new rpm_to_go_arch() helper to properly handle all
architecture names.

jobs/build-node-image.Jenkinsfile
@@ -184,7 +184,7 @@ lock(resource: "build-node-image") {
                                 cosa shell basename \$link
                             """)
                             shwrap("cosa decompress --build $build_id")
-                            def skopeo_arch_override = (arch == "x86_64") ? "amd64" : arch
+                            def skopeo_arch_override = rpm_to_go_arch(arch)
                             shwrap("cosa shell skopeo copy --override-arch ${skopeo_arch_override} --authfile $REGISTRY_AUTH_FILE docker://${registry_staging_repo}@${node_image_manifest_digest} oci-archive:./openshift-${arch}.ociarchive")
                             kola(
                                 cosaDir: WORKSPACE,

commit b109710ec55893998386aa795c375fe9cc0a81f1

jobs/build-node-image: call rpm_to_go_arch via pipeutils
Fix NoSuchMethodError by referencing rpm_to_go_arch through the loaded
pipeutils object.

Fixes: 8bae67d

jobs/build-node-image.Jenkinsfile
@@ -184,7 +184,7 @@ lock(resource: "build-node-image") {
                                 cosa shell basename \$link
                             """)
                             shwrap("cosa decompress --build $build_id")
-                            def skopeo_arch_override = rpm_to_go_arch(arch)
+                            def skopeo_arch_override = pipeutils.rpm_to_go_arch(arch)
                             shwrap("cosa shell skopeo copy --override-arch ${skopeo_arch_override} --authfile $REGISTRY_AUTH_FILE docker://${registry_staging_repo}@${node_image_manifest_digest} oci-archive:./openshift-${arch}.ociarchive")
                             kola(
                                 cosaDir: WORKSPACE,