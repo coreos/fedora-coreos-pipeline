92dc9592af24c04e9c1ddc733a395949b5e49f35
Huijing Hei <hhei@redhat.com>
2026-03-04 21:27:11 +0800

    libcloud: hardcode `aws_regions` without `me-central-1` due to AWS outage
    Inspired by Dusty's comment
    https://github.com/coreos/fedora-coreos-pipeline/pull/1307#issuecomment-3997475619
    
    Copy the aws regions from https://jenkins-rhcos--prod-pipeline.apps.int.prod-stable-spoke1-dc-iad2.itup.redhat.com/job/cloud-replicate/33/console

--- libcloud.groovy
+++ libcloud.groovy
@@ -42,6 +42,46 @@ def replicate_to_clouds(pipecfg, basearch, buildID, stream) {
             if (winli) {
                 extraArgs += "--winli"
             }
+            if (c.primary_region == "us-east-1") {
+                // Exclude "me-central-1" due to AWS outage
+                def aws_regions = [
+                    "af-south-1",
+                    "ap-east-1",
+                    "ap-east-2",
+                    "ap-northeast-1",
+                    "ap-northeast-2",
+                    "ap-northeast-3",
+                    "ap-south-1",
+                    "ap-south-2",
+                    "ap-southeast-1",
+                    "ap-southeast-2",
+                    "ap-southeast-3",
+                    "ap-southeast-4",
+                    "ap-southeast-5",
+                    "ap-southeast-6",
+                    "ap-southeast-7",
+                    "ca-central-1",
+                    "ca-west-1",
+                    "eu-central-1",
+                    "eu-central-2",
+                    "eu-north-1",
+                    "eu-south-1",
+                    "eu-south-2",
+                    "eu-west-1",
+                    "eu-west-2",
+                    "eu-west-3",
+                    "il-central-1",
+                    "me-south-1",
+                    "mx-central-1",
+                    "sa-east-1",
+                    "us-east-1",
+                    "us-east-2",
+                    "us-west-1",
+                    "us-west-2"
+                ]
+                def aws_regions_arg = aws_regions.join(" ")
+                extraArgs += "--regions=${aws_regions_arg}"
+            }
             shwrap("""
             cosa aws-replicate \
                 --log-level=INFO \
