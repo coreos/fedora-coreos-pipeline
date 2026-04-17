5ebb0faac06325b9ba52d452b56c44ff0ca56514
Roshan-R <rchandar@redhat.com>
2026-04-17 14:45:53 +0530

    chore: remove unnecessary cat

--- tmt/tests/init.sh
+++ tmt/tests/init.sh
@@ -28,8 +28,8 @@ echo "cosa container: $COREOS_ASSEMBLER_CONTAINER_LATEST"
 echo "arch: $(arch)"
 echo "using image: $IMAGE_URL"
 skopeo inspect "docker://$IMAGE_URL" > meta.json
-GIT_URL=$(cat meta.json | jq -r ".Labels.\"org.opencontainers.image.source\"")
-GIT_REF=$(cat meta.json | jq -r ".Labels.\"com.coreos.stream\"")
+GIT_URL=$(jq -r ".Labels.\"org.opencontainers.image.source\"" meta.json)
+GIT_REF=$(jq -r ".Labels.\"com.coreos.stream\"" meta.json)
 echo "export GIT_URL=${GIT_URL}" >> "$HOME/utils.sh"
 echo "export GIT_REF=${GIT_REF}" >> "$HOME/utils.sh"
 echo "git version: $(git --version)"
